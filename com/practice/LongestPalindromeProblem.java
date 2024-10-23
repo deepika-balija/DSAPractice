8b382bcb15885045310c1a79a92decad9cfd6032
deepika-balija <deepikabalija.maha@gmail.com>
2024-10-23 10:07:18 +0530

    LongestPalindromeProblem

---
+++ com/practice/LongestPalindromeProblem.java
@@ -0,0 +1,55 @@
+package com.practice;
+
+public class LongestPalindromeProblem {
+    public static void main(String[] args) {
+String input = "forgeeksskeegfor";
+        System.out.println(findingLongestPalindromeLength(input));
+    }
+public static int findingLongestPalindromeLength(String s) {
+        if(s == null || s.length() == 0)
+            return 0;
+
+       String lSP ="";
+       for(int i=1;i<s.length();i++){
+//for odd length
+           int low =i;
+           int high =i;
+           while(s.charAt(low)==s.charAt(high)){
+               low--;
+               high++;
+               //terminate the condition if we reachstart /end of string
+               if(low == -1 || high == s.length()){
+                   break;
+               }
+               ///indexes low & high can be used to extract the substring
+               String palindrome = s.substring(low+1,high);
+               if(palindrome.length() > lSP.length()){
+                   //capture the longest palindrome found
+                   lSP = palindrome;
+               }
+           }
+           // for even length
+            low=i-1;
+            high=i;
+
+           while(s.charAt(low)==s.charAt(high)){
+               low--;
+               high++;
+               if(low == -1 || high == s.length()){
+                   break;
+               }
+               String palindrome = s.substring(low+1,high);
+               if(palindrome.length() > lSP.length()){
+                   lSP = palindrome;
+               }
+           }
+
+       }
+    System.out.println(lSP);
+       return lSP.length();
+}
+
+
+
+
+}
