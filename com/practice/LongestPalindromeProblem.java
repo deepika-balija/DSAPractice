e42ecc034294f6895ff78c6e35c53a308507e5f9
deepika-balija <deepikabalija.maha@gmail.com>
2024-11-05 09:57:07 +0530

    Practice

--- com/practice/LongestPalindromeProblem.java
+++ com/practice/LongestPalindromeProblem.java
@@ -2,53 +2,50 @@ package com.practice;
 
 public class LongestPalindromeProblem {
     public static void main(String[] args) {
-String input = "forgeeksskeegfor";
+        String input = "forgeeksskeegfor";
         System.out.println(findingLongestPalindromeLength(input));
+
+        System.out.println(findingLongestPalindromeLength(input).length());
     }
-public static String findingLongestPalindromeLength(String s) {
-        if(s == null || s.length() == 0)
+
+    public static String findingLongestPalindromeLength(String s) {
+        if (s == null || s.length() == 0)
             return s;
-
-       String lSP ="";
-       for(int i=1;i<s.length();i++){
-//for odd length
-           int low =i;
-           int high =i;
-           while(s.charAt(low) == s.charAt(high)){
-               low--;
-               high++;
-               //terminate the condition if we reach start /end of string
-               if(low == -1 || high == s.length()){
-                   break;
-               }
-               ///indexes low & high can be used to extract the substring
-               String palindrome = s.substring(low+1,high);
-               if(palindrome.length() > lSP.length()){
-                   //capture the longest palindrome found
-                   lSP = palindrome;
-               }
-           }
-           // for even length
-            low=i-1;
-            high=i;
-
-           while(s.charAt(low)==s.charAt(high)){
-               low--;
-               high++;
-               if(low == -1 || high == s.length()){
-                   break;
-               }
-               String palindrome = s.substring(low+1,high);
-               if(palindrome.length() > lSP.length()){
-                   lSP = palindrome;
-               }
+//        int n = s.length() - 1;
+        int maxlen = 1;
+        int startindex = 0;
+        for (int i = 0; i < s.length()-1 ; i++) {
+            //for even length
+            int low = i;
+            int high = i + 1;
+            int len = expandAround(s, low, high);
+           if (len > maxlen) {
+               maxlen = len;
+               startindex = i -(maxlen-1)/2;
            }
 
-       }
-    System.out.println(lSP);
-       return lSP;
-}
 
+        }
+        for(int i=1; i< s.length()-1; i++){
+            int low = i-1;
+            int high = i+1;
+            int len = expandAround(s, low, high);
+            if (len > maxlen) {
+                maxlen = len;
+                startindex = i -(len-1)/2;
+            }
+
+        }
+        return s.substring(startindex, startindex+maxlen);
+    }
+        public static int expandAround(String s, int low, int high){
+            while (low>=0 &&  high < s.length() && s.charAt(low) == s.charAt(high)) {
+                low--;
+                high++;
+
+            }
+            return high-low-1;
+        }
 
 
 
