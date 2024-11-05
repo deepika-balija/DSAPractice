48c8696876bde6ef3ca48aad165ab403ac8b8b13
deepika-balija <deepikabalija.maha@gmail.com>
2024-11-05 17:12:14 +0530

    Practice

--- com/practice/PowerOfTenProblem.java
+++ com/practice/PowerOfTenProblem.java
@@ -2,7 +2,7 @@ package com.practice;
 
 public class PowerOfTenProblem {
 public static void main(String[] args) {
-    System.out.println(isPowerOfTen(1));
+    System.out.println(isPowerOfTen(999));
 }
 
 static boolean isPowerOfTen(int n) {
