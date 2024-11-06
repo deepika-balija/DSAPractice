1cba4ad700200e853be31f7468ba0ff733ebc0fd
deepika-balija <deepikabalija.maha@gmail.com>
2024-11-06 12:05:04 +0530

    Practice

---
+++ com/practice/ArrayOfStrings.java
@@ -0,0 +1,52 @@
+package com.practice;
+
+import java.util.*;
+
+public class ArrayOfStrings {
+    public static void main(String[] args) {
+        String[] dict = {"ab", "abcd", "bcdaf", "bcad", "dacb","acb", "acab"};
+        String input = "abcd";
+        System.out.println(Arrays.toString(findingArrayOfStrings(dict, input)));
+    }
+
+    private static String[] findingArrayOfStrings(String[] dict, String input) {
+        List<String> list = new ArrayList<>();
+
+         int inputFreqArr[] = new int[26];
+        for(int i = 0; i < input.length(); i++) {
+           char ch = input.charAt(i);
+           int index = ch - 'a';
+            inputFreqArr[index]++;
+        }
+ for(int i = 0; i < dict.length; i++) {
+     String s = dict[i];
+     int wordFreqArr[]= new int[26];
+     if(s.length() ==input.length()){
+         for(int j = 0; j < s.length(); j++) {
+
+             char ch = dict[i].charAt(j);
+             int index = ch - 'a';
+             wordFreqArr[index]++;
+         }
+         if(Arrays.equals(inputFreqArr,wordFreqArr)){
+             list.add(s);
+         }
+
+     }
+ }
+ return list.toArray(new String[0]);
+    }
+}
+
+//"Given a dictionary (list of words) and a word,
+//        return an array of strings that can be formed from the given word.
+//        [Every letter in the input word can occur only once in the word to be returned].
+//
+//Examples:
+//String[] dict = {""ab"", ""abcd"", ""bcdaf"", ""bcad"", ""acb"", ""acab""};
+//String input = "abcd";
+//// Return {""abcd"", ""bcad""}
+//
+//String[] dict =  {""ab"", ""abcd"", ""bcdaf"", ""bcad"", ""acaab"", ""acab""};
+//String input = ""caab"";
+//// Return {""acab""}"
